e1d2b1a902184de109a3051e659a464a305449bf
Samuel Bosawer <60697963+samuelbosawer@users.noreply.github.com>
2023-07-27 19:57:47 +0900

    Update distrikpdf.php

--- application/views/admin/wajibpajak/distrikpdf.php
+++ application/views/admin/wajibpajak/distrikpdf.php
@@ -28,9 +28,7 @@
                 <th>Alamat Usaha</th>
                 <th>Pemilik Usaha</th>
                 <th>Email Pemilik</th>
-                <th>No Telepon Pemilik</th>
                 <th>Jumlah Kamar (Usaha Kos)</th>
-                <th>Volumen Air</th>
             </tr>
             <?php $i=1;
             foreach($wajib_pajak_sortir_distrik as $pajak):
@@ -41,22 +39,7 @@
                     <td><?=$pajak["usaha_alamat"]?></td>
                     <td><?=$pajak["pemilik_nama"]?></td>
                     <td><?=$pajak["pemilik_email"]?></td>
-                    <td><?=$pajak["pemilik_telp"]?></td>
                     <td><?=$pajak["usaha_kos_jml_kmr"]?></td>
-                    <td>
-                        <?php
-                        $queryVolumeAir = $this->db->query("SELECT volume FROM tagihan WHERE id_wp = '".$pajak['id_wp']."' ORDER BY id DESC LIMIT 1" );
-                        $rowVolumeAir = $queryVolumeAir->row_array();
-                        if($rowVolumeAir == null)
-                        {
-                        echo "-";
-                        }else{
-                        echo volume_air($rowVolumeAir['volume']);
-                        }
-                        ?>
-                    </td>
-                </tr>
-            <?php endforeach ?>    
         </table>  
              
         <div class="ttd" style="text-align: center; margin-left:500px">
