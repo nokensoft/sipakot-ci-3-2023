16e58aaeed9d5807680d8716f7b2d0a3c3a108cf
Samuel Bosawer <zam.bosawer@gmail.com>
2023-11-21 22:56:49 +0900

    Updated halaman login

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -14,7 +14,7 @@ class Beranda extends CI_Controller
     public function index()
     {	
 		$pengaturan = array (
-			'beritas' => $this->M_berita->beranda_berita('berita')->result_array(),
+			// 'beritas' => $this->M_berita->beranda_berita('berita')->result_array(),
 			'judul_situs' => $this->M_pengaturan->judul_situs(),
 			'footer_text' => $this->M_pengaturan->footer_text(),
 			'footer_text_right' => $this->M_pengaturan->footer_text_right(),
